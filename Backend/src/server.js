06b071ce7c1b28fd661e5a5245d1df6f4175a8e9
Yashwanth Raju <yashu.raju005@gmail.com>
2026-04-29 10:50:15 +0530

    deployement

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
+import cors from "cors";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -11,21 +13,20 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(cors({
+  origin: "https://your-frontend.vercel.app", // update later
+  credentials: true
+}));
 
+app.use(express.json());
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoute)
 
-// make ready for deployment
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../../Frontend/dist/index.html"));
-    });
-}
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
 app.listen(process.env.PORT,()=>{
-    console.log("Server Running On Port 3000")
+    console.log(`Server Running On Port ${process.env.PORT}`)
 })
 
