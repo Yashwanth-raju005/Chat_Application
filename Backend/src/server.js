d25f1a0b598ff9cfb4a73f0d0c044c395578e80e
Yashwanth Raju <yashu.raju005@gmail.com>
2026-05-17 17:50:09 +0530

    SignUp Frontend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -15,13 +15,14 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({
-  origin: "https://chat-application-61p2v0v36-yashwanths-projects-18cdfc80.vercel.app", // update later
-  credentials: true
-}));
+// app.use(cors({
+//   origin: "https://chat-application-61p2v0v36-yashwanths-projects-18cdfc80.vercel.app", // update later
+//   credentials: true
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoute)
