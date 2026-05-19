9f073e068be1bc611e1cff9ae27a7e5b16c18353
Yashwanth Raju <yashu.raju005@gmail.com>
2026-05-19 13:39:01 +0530

    ChatPage Layout

--- Frontend/src/pages/ChatPage.jsx
+++ Frontend/src/pages/ChatPage.jsx
@@ -1,15 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import { useAuthStore } from "../store/useAuthStore.js";
+import React from "react";
+
+import BorderAnimatedContaner from "../components/BorderAnimatedContainer.jsx";
+import { useChatStore } from "../store/useChatStore.js";
+
+import ProfileHeader from "../components/ProfileHeader.jsx";
+import ActiveTabSwitch from "../components/ActiveTabSwitch.jsx";
+import ChatsList from "../components/ChatsList.jsx";
+import ContactList from "../components/ContactList.jsx";
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder.jsx";
 
 const ChatPage = () => {
-  const {logout} = useAuthStore()
+  const { activeTab,selectedUser } = useChatStore();
   return (
-    <div className='z-10'>
-      ChatPage
-      <button onClick={logout}>logout</button>
-    </div>
-  )
-}
+    <div className="relative w-full max-w-6xl h-[700px]">
+      <BorderAnimatedContaner>
+        {/* LEFT SIDE */}
 
-export default ChatPage
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+          {
+            selectedUser ? <ChatContainer/> : <NoConversationPlaceholder/>
+          }
+
+        </div>
+
+
+      </BorderAnimatedContaner>
+    </div>
+  );
+};
+
+export default ChatPage;
